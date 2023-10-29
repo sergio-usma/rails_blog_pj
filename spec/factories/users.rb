cc18d4e2e6cb46482f46bd1d2a85321147177843
Sergio Usma <sergioandresusma@hotmail.com>
2023-10-28 21:40:27 -0500

    Update users.rb factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :user do
-    name { 'John Doe' }
+    name { "John Doe" }
+    photo { "https://example.com/photo.jpg" }
+    bio { "User bio" }
     posts_counter { 0 }
   end
 end
+
