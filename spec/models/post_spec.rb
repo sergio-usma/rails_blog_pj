5ca60bc51e96945f6945b76b2eab9bce434c7872
Sergio Usma <sergioandresusma@hotmail.com>
2023-10-28 21:41:16 -0500

    FIX post_spec.rb testing

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,21 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'is valid with a title, comments_counter, likes_counter, author_id and author' do
-    user = User.create(name: 'John Doe')
-    post = Post.new(title: 'Lorem ipsum', comments_counter: 0, likes_counter: 0, author_id: user.id, author: user)
-    expect(post).to be_valid
+  describe 'associations' do
+    it { is_expected.to belong_to(:author).class_name('User') }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
+    it { is_expected.to have_many(:likes).dependent(:destroy) }
   end
 
-  it 'is invalid without a title' do
-    user = User.create(name: 'John Doe')
-    post = Post.new(comments_counter: 0, likes_counter: 0, author_id: user.id, author: user)
-    expect(post).to_not be_valid
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_length_of(:title).is_at_most(250) }
+    it { is_expected.to validate_numericality_of(:comments_counter).is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_numericality_of(:likes_counter).is_greater_than_or_equal_to(0) }
   end
 
-  it 'is invalid with a negative comments_counter' do
-    user = User.create(name: 'John Doe')
-    post = Post.new(title: 'Lorem ipsum', comments_counter: -1, likes_counter: 0, author_id: user.id, author: user)
-    expect(post).to_not be_valid
+  describe '#recent_comments' do
+    let(:post) { create(:post) }
+
+    it 'returns the 5 most recent comments' do
+      old_comments = create_list(:comment, 2, post: post, created_at: 2.days.ago)
+      new_comments = create_list(:comment, 3, post: post, created_at: 1.day.ago)
+      post.reload
+
+      expect(post.recent_comments.size).to eq(5)
+      expect(post.recent_comments).to eq(new_comments + old_comments)
+    end
+  end
+
+
+  describe '#update_post_counter' do
+    let(:user) { create(:user) }
+
+    it 'updates the posts_counter of author' do
+      create(:post, author: user)
+      user.reload
+      expect(user.posts_counter).to eq(1)
+    end
   end
 end
