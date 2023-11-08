2e2342063a6aa6120586f39fbe454aaeaf05743f
sergiok <sergioandresusma@hotmail.com>
2023-11-08 16:37:27 -0500

    Add handler to show method in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,20 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find_by(id: params[:user_id])
+    if @user
+      @posts = @user.posts
+      @user_name = @user.name
+    else
+      redirect_to users_path, alert: "User not found."
+    end
   end
 
   def show
-    @post = Post.find_by(id: params[:id])
-    @user = @post.author
-  end
-
-  def post
-    @user = User.find(params[:id])
-    @post = Post.find(params[:post_id])
+    @post = Post.find_by(id: params[:post_id])
+    if @post
+      @user = @post.author
+    else
+      redirect_to user_posts_path(params[:user_id]), alert: "Post not found."
+    end
   end
 end
