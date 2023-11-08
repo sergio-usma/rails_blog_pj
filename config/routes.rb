3b70576e0f1c40345c66d438d3e10e843736fcbb
sergiok <sergioandresusma@hotmail.com>
2023-11-08 16:36:36 -0500

    Update routes file according to requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   root 'users#index'
   get "up" => "rails/health#show", as: :rails_health_check
-  # resources :users, only: [:new, :create]
-  # Defines the root path route ("/")
-  # root "users#index"
-  get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/posts', to: 'users#posts', as: 'user_posts'
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/:user_id', to: 'users#show', as: 'user'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   get '/users/:id/posts/:post_id', to: 'posts#show', as: 'user_post'
 end
