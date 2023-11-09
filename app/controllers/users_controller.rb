01453435ada36b0305031959c7c09ccf78a276b8
sergiok <sergioandresusma@hotmail.com>
2023-11-09 10:36:42 -0500

    Add an action to show only the last three posts in the users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:user_id])
     if @user
       @posts = @user.posts
+      @recent_posts = @user.posts.order(created_at: :desc).limit(3)
     else
       redirect_to users_path, alert: 'User not found.'
     end
