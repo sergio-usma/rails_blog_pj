651c9cb0e08ce8cf765760303e2b210018f9b744
sergiok <sergioandresusma@hotmail.com>
2023-11-11 15:50:53 -0500

    Update users_controller show view to manage posts forms & data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:user_id])
-    if @user
-      @posts = @user.posts
-      @recent_posts = @user.posts.order(created_at: :desc).limit(3)
-    else
-      redirect_to users_path, alert: 'User not found.'
-    end
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 end
