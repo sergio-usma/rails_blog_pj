5579c258e8c32d6bafb33e80d75006f64d4fd02f
sergiok <sergioandresusma@hotmail.com>
2023-11-11 15:10:35 -0500

    Add new and create methods in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,4 +17,25 @@ class PostsController < ApplicationController
       redirect_to users_path, alert: 'Post not found.'
     end
   end
+
+  def new
+    @post = Post.new
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = params[:user_id]
+    if @post.save
+      redirect_to user_post_path(@post.author, @post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
