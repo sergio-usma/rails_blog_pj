8a2019b52ccd56994ad81f7e897a226eb725de86
sergiok <sergioandresusma@hotmail.com>
2023-11-17 16:02:03 -0500

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,8 @@ first_user = User.create(
   'password' => 'password',
   'photo' => 'https://i.pravatar.cc/150?img=3',
   'bio' => 'Michael is a distinguished historian specializing in medieval Europe. Born in London in 1978, he developed an interest in history at a young age. He earned his Ph.D. in Medieval Studies from Oxford University and has since published several acclaimed books on the Crusades. Michael is also known for his engaging lectures and commitment to making history accessible to a wider audience.',
-  'confirmed_at' => Time.now)
+  'confirmed_at' => Time.now,
+  'role' => 'user')
 
 second_user = User.create(
   'name' => 'Sarah Patel',
@@ -23,7 +24,9 @@ second_user = User.create(
   'password' => 'password',
   'photo' => 'https://i.pravatar.cc/150?img=25',
   'bio' => 'Sarah is an influential software engineer and tech entrepreneur from Mumbai, born in 1982. She co-founded a leading AI startup that revolutionized machine learning applications in healthcare. Sarah is a prominent advocate for women in technology and has been featured in numerous tech conferences as a keynote speaker.',
-  'confirmed_at' => Time.now)
+  'confirmed_at' => Time.now,
+  'role' => 'admin')
+
 
 third_user = User.create(
   'name' => 'Nina Nguyen',
@@ -31,7 +34,8 @@ third_user = User.create(
   'password' => 'password',
   'photo' => 'https://i.pravatar.cc/150?img=20',
   'bio' => 'Nina is a Vietnamese-American film director born in 1976 in Ho Chi Minh City. She moved to Los Angeles as a child and later attended NYU\'s Tisch School of the Arts. Her films, known for their powerful storytelling and visual artistry, have won multiple awards at international film festivals.',
-  'confirmed_at' => Time.now)
+  'confirmed_at' => Time.now,
+  'role' => 'user')
 
 fourth_user = User.create(
   'name' => 'Emma Johnson',
@@ -39,7 +43,8 @@ fourth_user = User.create(
   'password' => 'password',
   'photo' => 'https://i.pravatar.cc/150?img=47',
   'bio' => 'Emma is a Canadian environmental scientist born in Toronto in 1985. Her groundbreaking research on climate change and its impact on Arctic ecosystems has been widely recognized. Emma is an advocate for sustainable practices and has worked with various NGOs to promote environmental awareness and policy change.',
-  'confirmed_at' => Time.now)
+  'confirmed_at' => Time.now,
+  'role' => 'user')
 
 ### Posts ###
 first_post = Post.create(author: first_user, 'title' => 'Post 1', 'text' => 'This is the text of post 1')
