d0b2d7a2f5b77d40627712e2b082df33538bdcd5
sergiok <sergioandresusma@hotmail.com>
2023-11-16 19:22:22 -0500

    FIX migration issue in email required field

--- db/migrate/20231028133656_blog_users.rb
+++ db/migrate/20231028133656_blog_users.rb
@@ -2,7 +2,7 @@ class BlogUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :email, null:false, unique: true
+      t.string :email, null:false
       t.string :photo
       t.text :bio
       t.integer :posts_counter, default: 0
